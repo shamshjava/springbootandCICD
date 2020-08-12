a1d834082cc7c416959ea5a59d6b5022921d1303
shamshir <shamshir.alam@gmil.com>
2020-08-12 17:31:49 +0530

    updated controller

--- src/main/java/com/alam/springbootwithcicd/controller/MyController.java
+++ src/main/java/com/alam/springbootwithcicd/controller/MyController.java
@@ -1,14 +1,13 @@
 package com.alam.springbootwithcicd.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/springbootcicd")
+//@RequestMapping(value="/springbootcicd")
 public class MyController {
 	
-	@GetMapping("/")
+	@GetMapping("/springbootcicd")
 	public String sayHello() {
 		return "Hello World";
 	}
