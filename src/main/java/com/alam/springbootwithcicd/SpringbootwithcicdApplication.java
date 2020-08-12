1a9c54fbe3f0dbcdb887615225bde7e718da5f00
shamshir <shamshir.alam@gmil.com>
2020-08-12 16:43:16 +0530

    updated SpringbootwithcicdApplication.java file

--- src/main/java/com/alam/springbootwithcicd/SpringbootwithcicdApplication.java
+++ src/main/java/com/alam/springbootwithcicd/SpringbootwithcicdApplication.java
@@ -2,12 +2,19 @@ package com.alam.springbootwithcicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootwithcicdApplication {
+public class SpringbootwithcicdApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootwithcicdApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringbootwithcicdApplication.class);
+	}
 }
