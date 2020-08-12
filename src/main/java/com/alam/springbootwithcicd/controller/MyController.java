a4ecc929a7ab0c08bf1e6a1a8d2bf487f3f41a74
shamshir <shamshir.alam@gmil.com>
2020-08-12 13:54:00 +0530

    Change mapping string

--- src/main/java/com/alam/springbootwithcicd/controller/MyController.java
+++ src/main/java/com/alam/springbootwithcicd/controller/MyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 	
-	@GetMapping("/")
+	@GetMapping("/springbootcicd")
 	public String sayHello() {
 		return "Hello World";
 	}
